3eed699cb3ac3e93a641b796fe7b660430b0db7f
cequella <pedro.oliveira.demolay@gmail.com>
2017-05-23 15:31:52 -0300

    Drawing pseudo-angle

--- MFPG2017.1/PseudoAngleViewer/js/App.js
+++ MFPG2017.1/PseudoAngleViewer/js/App.js
@@ -42,12 +42,124 @@ App.prototype.drawGrid = function(){
     drawer.rect(this.square.x, this.square.y, this.square.size, this.square.size);
 }
 App.prototype.pseudoAngle = function(){
-    var drawer  = this.drawer;
+    var   drawer = this.drawer;
+    const size   = this.squareSize;
+    const hSize  = this.squareSize/2.0;
 
-    drawer.lineWidth(4)
-	.strokeStyle("red")
-	.line(drawer.centerX(-this.squareSize), drawer.centerY(), drawer.centerX(-this.squareSize), this.squareSize);
-    drawer.circle(drawer.centerX(-this.squareSize), this.squareSize, 2);
+    var value = -0.8;
+    if(value >= 8.0){
+	value = value%8.0;
+    } else if(value < 0.0){
+	value = 8.0-Math.abs(value);
+	value = value%8.0;
+    }
+
+    drawer.lineWidth(4).strokeStyle("red");
+    
+    if(value>0){
+	const temp = Math.min(1.0, value);
+	
+	drawer.line(drawer.centerX(-size),
+		    drawer.centerY(),
+		    drawer.centerX(-size),
+		    drawer.centerY()-hSize*temp);
+
+	if(value <= 1){
+	    drawer.circle(drawer.centerX(-size),
+			  drawer.centerY()-hSize*temp, 2);
+	}
+    } else return;
+    if(value>1){
+	const temp = Math.min(1.0, value-1.0);
+	
+	drawer.line(drawer.centerX(-size),
+		    drawer.centerY(size),
+		    drawer.centerX(-size)-hSize*temp,
+		    drawer.centerY(size));
+
+	if(value <= 2){
+	    drawer.circle(drawer.centerX(-size)-hSize*temp,
+			  drawer.centerY(size), 2);
+	}
+    } else return;
+    if(value>2){
+	const temp = Math.min(1.0, value-2.0);
+	
+	drawer.line(drawer.centerX(),
+		    drawer.centerY(size),
+		    drawer.centerX()-hSize*temp,
+		    drawer.centerY(size));
+
+	if(value <= 3){
+	    drawer.circle(drawer.centerX()-hSize*temp,
+			  drawer.centerY(size), 2);
+	}
+    } else return;
+    if(value>3){
+	const temp = Math.min(1.0, value-3.0);
+	
+	drawer.line(drawer.centerX(size),
+		    drawer.centerY(size),
+		    drawer.centerX(size),
+		    drawer.centerY(size)+temp*hSize);
+
+	if(value <= 4){
+	    drawer.circle(drawer.centerX(size),
+			  drawer.centerY(size)+temp*hSize, 2);
+	}
+    } else return;
+    if(value>4){
+	const temp = Math.min(1.0, value-4.0);
+	
+	drawer.line(drawer.centerX(size),
+		    drawer.centerY(),
+		    drawer.centerX(size),
+		    drawer.centerY()+hSize*temp);
+
+	if(value <= 5){
+	    drawer.circle(drawer.centerX(size),
+			  drawer.centerY()+hSize*temp, 2);
+	}
+    } else return;
+    if(value>5){
+	const temp = Math.min(1.0, value-5.0);
+	
+	drawer.line(drawer.centerX(size),
+		    drawer.centerY(-size),
+		    drawer.centerX(size)+hSize*temp,
+		    drawer.centerY(-size));
+	
+	if(value <= 6){
+	    drawer.circle(drawer.centerX(size)+hSize*temp,
+			  drawer.centerY(-size), 2);
+	}
+    } else return;
+    if(value>6){
+	const temp = Math.min(1.0, value-6.0);
+	
+	drawer.line(drawer.centerX(),
+		    drawer.centerY(-size),
+		    drawer.centerX()+hSize*temp,
+		    drawer.centerY(-size));
+
+	if(value <= 7){
+	    drawer.circle(drawer.centerX()+hSize*temp,
+			  drawer.centerY(-size), 2);
+	}
+    } else return;
+    if(value>7){
+	const temp = Math.min(1.0, value-7.0);
+	
+	drawer.line(drawer.centerX(-size),
+		    drawer.centerY(-size),
+		    drawer.centerX(-size),
+		    drawer.centerY(-size)-hSize*temp);
+
+	if(value <= 8){
+	    drawer.circle(drawer.centerX(-size),
+			  drawer.centerY(-size)-hSize*temp, 2);
+	}
+    } else return;
 }
 App.prototype.drawCursor = function(){
     var drawer  = this.drawer;
